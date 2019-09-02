fbec2404e723b1b9344175982f83579c2e22de08
auzadventure@gmail.com <auzadventure@gmail.com>
2019-09-02 23:48:05 +0800

    tighten the code a bit

--- Curlme.php
+++ Curlme.php
@@ -8,10 +8,10 @@ $Curlme = new Curlme;
 $Curlme->setBase('https://reqres.in');
 $Curlme->SetBase('http://localhost/local/php/fatfree');
 
-
-$params = [ 'name'=>'neo',
+$params = [ 
+			'name'=>'neo',
 			'job'=>'the two',
-			];
+		   ];
 
 print($Curlme->post('/post/create',$params));
 */ 
@@ -53,33 +53,23 @@ class Curlme {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		if($params !=='') {
 			$query = http_build_query($params);
-			curl_setopt($ch, CURLOPT_POSTFIELDS,$query );
+			curl_setopt($ch, CURLOPT_POSTFIELDS,$query);
 		}
 		$output = curl_exec ($ch);
 		curl_close ($ch);
 		return $output;		
-	
 	}
 	
 	private function setOperation($ch,$operation) {
-		
 		switch ($operation) {
-					
 			case 'post':
 				curl_setopt($ch, CURLOPT_POST,1);
 				break;
 			case 'put':
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 				break;
-				
 			case 'get':
-			default:
-				break;
 		}
-		
 	}
-
-	
 }
 
-?>
